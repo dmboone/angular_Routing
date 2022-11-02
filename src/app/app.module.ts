1d248e45d95222066c9a4528cc7db0fe74ccd9b4
Destiny Boone <destaj.db@gmail.com>
2022-11-02 10:21:06 -0400

    setting up and loading routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,22 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [ // contains our routes to different 'pages'
+  {
+    path: '', // empty would mean home; localhost:4200
+    component: HomeComponent
+  }, 
+  { 
+    path: 'users', // will translate to localhost:4200/users
+    component: UsersComponent // which component.html file will be shown at this path
+  },
+  {
+    path: 'servers',
+    component: ServersComponent
+  }
+];
 
 @NgModule({
   declarations: [
@@ -25,6 +41,7 @@ import { ServersService } from './servers/servers.service';
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(appRoutes), // need to import; .forRoot registers our routes in this Angular app
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
