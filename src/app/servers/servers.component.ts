4dd3b9233130722f39d382a7aee5110a5a7412a9
Destiny Boone <destaj.db@gmail.com>
2022-11-02 13:23:41 -0400

    using relative paths in programmatic navigation

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ServersService } from './servers.service';
 
 @Component({
@@ -9,10 +10,17 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   public servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, 
+              private router: Router,
+              private route: ActivatedRoute) { // injects the currently active route
 
-  ngOnInit() {
+  }
+
+  ngOnInit(){
     this.servers = this.serversService.getServers();
   }
 
+  onReload(){
+    this.router.navigate(['/servers'], {relativeTo: this.route}); // navigate method needs current active route as well to work if going to a different page
+  }
 }
