a55bbe0fb226b7837912276ef68228fba3065c23
Destiny Boone <destaj.db@gmail.com>
2022-11-02 13:56:13 -0400

    destroying route observables

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
+  paramsSubscription: Subscription
 
   constructor(private route: ActivatedRoute) { }
 
@@ -16,7 +18,7 @@ export class UserComponent implements OnInit {
       id: this.route.snapshot.params['id'], // grabs the id from the route we defined in app.module
       name: this.route.snapshot.params['name']
     };
-    this.route.params
+    this.paramsSubscription = this.route.params
       .subscribe( // this is an observable; allows for us to asynchronously execute code in the event that something occurs; 
                   // in this case if the parameters of the route (id and name) change, so if a different id and/or name are selected
         (params: Params)=>{ // takes in the updated parameters (id and name; we defined the name of these parameters in the app module)
@@ -24,5 +26,10 @@ export class UserComponent implements OnInit {
           this.user.name = params['name'];
         }
       );
+  }
+
+  ngOnDestroy(): void { // if you create your own observables you must do this part, but for predefined ones like .subscribe, this is actually done automatically 
+                        // but this is to reference what it would look like
+    this.paramsSubscription.unsubscribe();
   }
 }
