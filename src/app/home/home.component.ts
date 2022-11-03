bfd1eed8ca40035e17274c6224cdc0f7d32c293d
Destiny Boone <destaj.db@gmail.com>
2022-11-03 16:07:34 -0400

    using a fake auth service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,7 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router) { } // inject the router
+  constructor(private router: Router, private authService: AuthService) { } // inject the router and auth service
 
   ngOnInit() {
   }
@@ -16,5 +17,15 @@ export class HomeComponent implements OnInit {
   onLoadServer(id: number){
     this.router.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: '1'}, fragment: 'loading'}); // allows us to navigate to the servers route within a method
     // the slash gives us an absolute path
+  }
+
+  onLogin(){
+    this.authService.login();
+    alert('logged in. can now access child servers');
+  }
+
+  onLogout(){
+    this.authService.logout();
+    alert('logged out. cannot access child servers');
   }
 }
