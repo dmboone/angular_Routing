58804c0a04d4d5b1d6ea2a3ee0248bbb490cadd1
Destiny Boone <destaj.db@gmail.com>
2022-11-03 14:24:19 -0400

    using and handling query parameters

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -12,7 +12,7 @@ export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
   constructor(private serversService: ServersService,
-    private route: ActivatedRoute) { }
+    private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     const id = +this.route.snapshot.params['id']; //the plus converts the id from a string to a number, which is what it should be otherwise the link won't work
@@ -25,4 +25,8 @@ export class ServerComponent implements OnInit {
       );
   }
 
+  onEdit(){
+    this.router.navigate(['edit'], {relativeTo: this.route, queryParamsHandling: 'preserve'}); // appends edit to the current route (relativeTo) and navigates to it
+                                                                                              // preserve allows us to save our queryParams even when clicking edit
+  }
 }
